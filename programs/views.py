6711cb849fa0dfa9a140185b147ec63c15afe766
Ahmedaboujeeb <68650816+Ahmedaboujeeb@users.noreply.github.com>
2021-06-01 09:51:00 +0200

    Added programs view, url and programs.html

--- programs/views.py
+++ programs/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Program
 
-# Create your views here.
+
+def all_programs(request):
+
+    programs = Program.objects.all()
+
+    context = {
+        'programs': programs, 
+    }
+
+    return render(request, 'programs/programs.html', context)
