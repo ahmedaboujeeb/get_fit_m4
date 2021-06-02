c51be09eadd937312acf68da8e50f82f00707c87
Ahmedaboujeeb <68650816+Ahmedaboujeeb@users.noreply.github.com>
2021-06-02 13:27:32 +0200

    Added insight field to program model

--- programs/models.py
+++ programs/models.py
@@ -5,6 +5,7 @@ from django.db.models.fields import TextField
 class Program(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
+    insight = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
