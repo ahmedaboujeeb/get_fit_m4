ba6f3471e923e6370f6ad7f05d5c0be73e8994eb
Ahmedaboujeeb <68650816+Ahmedaboujeeb@users.noreply.github.com>
2021-06-03 14:12:53 +0200

    Created checkout model

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,32 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+
+class order(models.Model):
+    order_number = models.CharField(max_length=16, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=255, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = CountryField(null=False, blank=False)
+    postcode = models.CharField(max_length=10, null=True, blank=True)
+    town_or_city = models.CharField(max_length=30, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    total_amount = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    status = models.CharField(max_length=50, null=False, blank=False, default='not_paid')
+
+    def _generate_order_number(self):
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
