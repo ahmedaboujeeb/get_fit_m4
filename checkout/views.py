893fbc129d1d20a31427835be20064b78b2067d3
Ahmedaboujeeb <68650816+Ahmedaboujeeb@users.noreply.github.com>
2021-06-04 11:00:07 +0200

    Added Stripe sumbit form

--- checkout/views.py
+++ checkout/views.py
@@ -1,23 +1,42 @@
 from checkout.forms import OrderForm
 from django.shortcuts import render, get_object_or_404
+from django.conf import settings
+from django.contrib import messages
 
 from programs.models import Program
 from checkout.models import Order
 
 from .forms import OrderForm
 
+import stripe
+
 
 def checkout(request, program_id):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     program = get_object_or_404(Program, pk=program_id)
 
+    total = program.price
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'program': program,
-        'stripe_public_key': 'pk_test_51IpsIQIHH9h4Wbq5E3eRjo6XfyXRqLLOj0iYIwqd4K3bUgfpTLPIgEFkhNQCxP5wxTDh8yhh9E3fmyA3XRaQ4Sze00WwH9Doxa',
-        'client_secret': 'sk_test_51IpsIQIHH9h4Wbq5TMJSoJ3Epw5Nkxw1GgrQZShMf5aqQeZuCP1p81tGZEtOff5NBfZfOZM5Zsdt3RNx1kWPQnGK005fsKdYUK',
+        'total': total,
+        'stripe_public_key': 'stripe_public_key',
+        'client_secret': 'stripe_secret_key',
     }
 
     return render(request, template, context)
