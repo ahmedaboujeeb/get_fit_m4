e3b668ee1931b627df40c41a4f5e51d898bfd1c7
Ahmedaboujeeb <68650816+Ahmedaboujeeb@users.noreply.github.com>
2021-06-04 09:02:29 +0200

    Added Stripe JS and styling

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,21 @@
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#ff0000',
+        iconColor: '#ff0000'
+    }
+};
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request, program_id):
     context = {
         'order_form': order_form,
         'program': program,
+        'stripe_public_key': 'pk_test_51IpsIQIHH9h4Wbq5E3eRjo6XfyXRqLLOj0iYIwqd4K3bUgfpTLPIgEFkhNQCxP5wxTDh8yhh9E3fmyA3XRaQ4Sze00WwH9Doxa',
+        'client_secret': 'sk_test_51IpsIQIHH9h4Wbq5TMJSoJ3Epw5Nkxw1GgrQZShMf5aqQeZuCP1p81tGZEtOff5NBfZfOZM5Zsdt3RNx1kWPQnGK005fsKdYUK',
     }
 
     return render(request, template, context)
