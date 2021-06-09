cb5342827bddd2602ae5f0eec08c47b69da01db3
Ahmedaboujeeb <68650816+Ahmedaboujeeb@users.noreply.github.com>
2021-06-09 07:59:19 +0200

    Setup secret key in heroku

--- get_fit/settings.py
+++ get_fit/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7j1yh0=2iwf%bu4g8@m^@g03-k+1wss@ue2$8jvpwle14_sl=-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['getfit-m4.herokuapp.com', 'localhost']
 
