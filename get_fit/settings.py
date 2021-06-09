8fe1a118174d6d92820bb573e18146c0e5b35bde
Ahmedaboujeeb <68650816+Ahmedaboujeeb@users.noreply.github.com>
2021-06-09 16:41:26 +0200

    creted s3 bucket and setup setting for static

--- get_fit/settings.py
+++ get_fit/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
     #others
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -111,20 +112,10 @@ WSGI_APPLICATION = 'get_fit.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
+# https://docs.djangoproject.com/en/3.1/ref/settings/#database
+DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -167,6 +158,27 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2050 10:00:00 GMT',
+        'CacheControl': 'max-age=1000000',
+    }
+
+    AWS_STORAGE_BUCKET_NAME = 'getfit-m4'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+     # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 #Stripe
 STRIPE_CURRENCY = 'usd'
