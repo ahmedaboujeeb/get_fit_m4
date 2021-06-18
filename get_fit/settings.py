29e29b95bd77b12e79108b60fbd2072affbaac15
Ahmedaboujeeb <68650816+Ahmedaboujeeb@users.noreply.github.com>
2021-06-18 15:41:50 +0200

    created profile template and altered allauth templates

--- get_fit/settings.py
+++ get_fit/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'home',
     'programs',
     'checkout',
+    'profiles',
 
     #others
     'crispy_forms',
--- get_fit/urls.py
+++ get_fit/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('programs/', include('programs.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
