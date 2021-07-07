0d16279e8b698b0a0da1ee2cf8d628fcd6ce9dd1
Ahmedaboujeeb <68650816+Ahmedaboujeeb@users.noreply.github.com>
2021-07-07 11:55:13 +0200

    added access to my programs

--- programs/urls.py
+++ programs/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_programs, name='programs'),
-    path('<program_id>', views.program_info, name='program_info'),
+    path('<int:program_id>', views.program_info, name='program_info'),
+    path('my_programs/', views.my_programs, name='my_programs'),
 ]
--- programs/views.py
+++ programs/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 from .models import Program
+from checkout.models import Order
+from profiles.models import UserProfile
 
 
+@login_required
 def all_programs(request):
 
     programs = Program.objects.all()
@@ -13,12 +19,32 @@ def all_programs(request):
     return render(request, 'programs/programs.html', context)
 
 
+@login_required
 def program_info(request, program_id):
+    #Check if program is purchased by user
+    profile = UserProfile.objects.get(user=request.user)
+    orders = Order.objects.filter(program_id=program_id, user_profile=profile)
+    if orders.count() == 0:
+        messages.error(request, 'You have not bought this program')
+        return redirect(reverse('programs'))
+    else:
+        order = orders.first()
+        if order.status == 'paid':
+            program = Program.objects.get(pk=program_id)
+            context = {
+                'program': program
+            }
+            return render(request, 'programs/program_info.html', context)
 
-    program = get_object_or_404(Program, pk=program_id)
+
+@login_required
+def my_programs(request):
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    orders = profile.orders.all()
 
     context = {
-        'program': program, 
+        'orders': orders,
     }
 
-    return render(request, 'programs/program_info.html', context)
+    return render(request, 'programs/my_programs.html', context)
