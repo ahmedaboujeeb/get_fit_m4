6a2e64a3a9f86833b0fea3970e789109f3566bcf
Ahmedaboujeeb <68650816+Ahmedaboujeeb@users.noreply.github.com>
2021-07-19 20:19:49 +0200

    Added toasts and styling

--- get_fit/settings.py
+++ get_fit/settings.py
@@ -89,6 +89,8 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
@@ -113,10 +115,17 @@ WSGI_APPLICATION = 'get_fit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#database
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
